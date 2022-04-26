ce3506d46eccad4cfc93f67618995825a00e1784
Hfn <55591161+titocs@users.noreply.github.com>
2022-04-26 12:05:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const rating = document.querySelectorAll("ul > li");
+const valueRating = document.querySelectorAll("ul > li > p");
+const button = document.querySelector("button");
+const mainContent = document.querySelector("main");
+
+let Star;
+for(let i=0; i<rating.length; i++){
+    rating[i].addEventListener("click", function(){
+        for(var j=0; j<rating.length; j++){
+            if(rating[j].classList.contains("clicked") && valueRating[j].classList.contains("fontClicked")){
+                rating[j].classList.remove("clicked");
+                valueRating[j].classList.remove("fontClicked");
+            }
+        }
+        rating[i].classList.add("clicked");
+        valueRating[i].classList.add("fontClicked");
+        Star = i;
+    })
+}
+
+button.addEventListener("click", function(){
+    if(Star === undefined){
+        alert("Select rating first!");
+    }
+    else{
+        mainContent.innerHTML = `
+        <div class="say-thanks">
+            <img src="images/illustration-thank-you.svg" alt="thx">
+            <div class="notif">
+                <p>You selected ${Star + 1} out of 5</p>
+            </div>
+            <h1>Thank You!</h1>
+            <p>We appreciate you taking the time to give a rating. If you ever need more support, 
+                don't hesitate to get in touch!
+            </p>
+        </div>
+        `
+    }
+})
